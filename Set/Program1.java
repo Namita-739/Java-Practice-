9a4f560a512d9c4d05662b1313c12cb05769d29c
Namita-739 <namitagorai739@gmail.com>
2026-04-27 10:49:04 +0530

    Add Set demonstration in Program1
    Updated the Program1 class to include a Set demonstration.

--- Set/Program1.java
+++ Set/Program1.java
@@ -1,5 +1,18 @@
-public class Program1 {
+import java.util.*;
+
+class Geeks{
     public static void main(String[] args) {
-        System.out.println("Prohram1");
+        
+        Set<Object> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(20);
+        set.add("GfG");
+        set.add(true);
+
+        System.out.println(set);
     }
 }
+
+
+//[20, GfG, 10, true]
